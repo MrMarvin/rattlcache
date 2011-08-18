93bcc681103b2cab1ff8ef69cf5b43415bebd26b
Marv Cool <marv@hostin.is>
2011-08-19 00:37:09 +0200

    Cache: added sanitite() and sortParams()

--- lib/rattlecache.rb
+++ lib/rattlecache.rb
@@ -1,21 +1,42 @@
 require 'backend_manager'
+require 'net/http'
 
 module Rattlecache
 
   class Cache
-    
+
     def initialize(backend = :filesystem)
       @backend = Rattlecache::Backend.fetch(backend)
     end
-    
-    def get(url)
-      @backend.get()
+
+    def get(objectKey)
+      puts "Cache class gets you: #{objectKey}"
+      @backend.get(sanitize(objectKey))
     end
-    
+
     def post(object)
-      @backend.post(object)
+      puts "Cache class gets you: #{objectKey}"
+      @backend.post({sanitize(object[:key]),object[:data]})
     end
-    
+
+    def sanitize(objectKey)
+      # strip scheme, sort paramters and encode URL style
+      urlObj = URI.parse(objectKey)
+      key = urlObj.host
+      key << urlObj.path
+      key << sortParams(urlObj.query)
+      URI.escape(key)
+    end
+
+    def sortParams(query)
+      q = Hash.new
+      query.split("&").each do |parampair|
+        q[parampair.split("=")[0]] = parampair.split("=")[1]
+      end
+      s = Array.new
+      q.sort.each { |pair| s << pair.join("=")}
+      "?"+s.join("&")
+    end
+
   end
-  
 end
