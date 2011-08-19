9b6f0008d5f8b8a8ba5d6d03d682f9fe139e4672
Marv Cool <marv@hostin.is>
2011-08-19 17:33:19 +0200

    backends: moves needs_request?() to Cache class

--- lib/backends/filesystem.rb
+++ lib/backends/filesystem.rb
@@ -1,29 +1,46 @@
+require 'json'
+require 'base64'
+
 module Rattlecache
   module Backend
-    class Filesystem
+    class Filesystem < Cache
 
       def initialize(prefix = "/tmp/rattlecache/")
         @prefix = prefix
       end
 
       def get(objectKey)
+        #puts "Filesystem gets you: #{objectKey}"
         # get the file from the filesystem
+        ret = {:status => 404, :lastModified => nil, :object => nil}
         begin
           f = open_file(objectKey)
+          firstline = f.first
           object = f.read
           mtime = f.mtime
           f.close
-          {:status => 200, :lastModified => mtime, :object => object}
+          
+          unless needs_request?(Base64.strict_decode64(firstline.chomp),mtime)
+            # If the file needs to be re-requested, simply return 404.
+            # note: the first line is encoded in strict base64, so there are no \n's in it.
+            # However, reading it from file comes with a CLRF at the end of the line,
+            # which the strict_decode doesnt like. Chomp it!
+            ret = {:status => 200, :object => object}
+          end
         rescue Errno::ENOENT
-          {:status => 404, :lastModified => nil, :object => nil}
         end
+        return ret
       end
 
       def post(object)
         # put the file to the filesysten
+        firstline = Base64.strict_encode64(object[:header].to_json)
+        #puts "Debug: putting headers as firstline: #{firstline}"
         f = open_file(object[:key],"w")
+        f.puts(firstline)
         f.puts(object[:data])
         f.close
+        #puts "Debug: filesystem posted #{object[:key]}"
       end
 
       def open_file(objectKey,how="r")
--- lib/rattlecache.rb
+++ lib/rattlecache.rb
@@ -10,14 +10,33 @@ module Rattlecache
       @backend = Rattlecache::Backend.fetch(backend)
     end
 
+    @request_pragmas = {
+      "guild" => "Check if any fields are requested. If yes, request without fields and look into ['lastModified'] if we need to fetch new or answer with out cached result.",
+      "data" => "Everything data related should be considered stable. Cache it for one week.",
+      "auction" => "dont know yet...",
+      "else" => "cache it for as long as 'RETRY-AFTER'-responseheader told us. (600 sec)"
+    }
+
     def get(objectKey)
-      puts "Cache class gets you: #{objectKey}"
+      #puts "Cache class gets you: #{objectKey}"
       @backend.get(sanitize(objectKey))
     end
 
     def post(object)
-      puts "Cache class puts: #{object[:key]}"
-      @backend.post({:key => sanitize(object[:key]),:data => object[:data]})
+      #puts "Cache class puts: #{object[:key]}"
+      @backend.post({:key => sanitize(object[:key]), :header => object[:header], :data => object[:data]})
+    end
+    
+    def needs_request?(headerline,mtime)
+      header = JSON.parse(headerline)
+      #header["date"][0] is a String with CGI.rfc1123_date() encoded time, 
+      # as there is no easy method to inverse this coding, I will keep using the files mtime
+      # to estimate when the data was last recieved.
+      unless header["retry-after"].nil?
+        mtime+(header["retry-after"][0].to_i) < Time.now()
+      else
+        mtime+(60*60*24*7) < Time.now()
+      end
     end
 
     def sanitize(objectKey)
